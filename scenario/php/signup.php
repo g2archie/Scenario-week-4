6058492910e64c7d3ff1fe4084966ac3ef2ffd18
g2archie <g2archie@gmail.com>
2017-01-11 18:34:08 +0000

    update it

--- scenario/php/signup.php
+++ scenario/php/signup.php
@@ -1,13 +1,13 @@
 <?php
 //链接数据库服务器
-// echo (!!@mysql_connect("localhost","root","harry63779588"));
+// echo (!!@mysqli_connect("localhost","root","harry63779588"));
 $connect=mysql_connect("localhost","root","harry63779588");
-mysql_select_db("Scenario4", $connect) or die('数据库连接错误，错误信息：'.mysql_error()); //链接到Scenario4数据库
+mysqli_select_db("Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 // $query1="INSERT INTO userinfo (Username, Password) VALUES ('allen','123')";
 
 // $result=mysql_query($query1) or die('sql错误'.mysql_error());
 
-mysql_query("INSERT INTO userinfo (Username, Password) VALUES ('$_POST[user_name]','$_POST[password]')"); //将信息添加到userinfo这个表
+mysqli_query($connect,"INSERT INTO userinfo (Username, Password) VALUES ('$_POST[user_name]','$_POST[password]')"); //将信息添加到userinfo这个表
 
 // $result = mysql_query("SELECT * FROM userinfo");  //显示所有数据
 // while($row = mysql_fetch_array($result))
@@ -20,6 +20,6 @@ mysql_query("INSERT INTO userinfo (Username, Password) VALUES ('$_POST[user_name
 // echo "1 record added";
 header('Location:../pages/signin.html');
 
-mysql_close($connect);
+mysqli_close($connect);
 
 ?>
