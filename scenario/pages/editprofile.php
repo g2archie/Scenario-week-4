adc804b54fdbc1c77ad7980b303753024aa84c79
harrychen0702 <chenhao201509@163.com>
2017-01-12 17:35:19 +0000

    fix html->php

--- scenario/pages/editprofile.php
+++ scenario/pages/editprofile.php
@@ -22,7 +22,7 @@ session_start();
         <ul class="left hide-on-med-and-down">
             <li><a href="home.php">Home</a></li>
             <li><a href="mysnippets.html">My Snippets</a></li>
-            <li><a href="newsnippet.html">New Snippets</a></li>
+            <li><a href="newsnippet.php">New Snippets</a></li>
             <li><a href="upload.html">Upload</a></li>
         </ul>
         <ul class="right hide-on-med-and-down">
