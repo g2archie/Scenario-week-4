00eaea734822370c07dd9179e7327551db90477e
harrychen0702 <chenhao201509@163.com>
2017-01-12 20:26:42 +0000

    fix

--- scenario/php/newsnippet.php
+++ scenario/php/newsnippet.php
@@ -13,14 +13,9 @@ $result=mysqli_query($connect,$query)or die(mysqli_error());
 $row=mysqli_fetch_row($result);
 $ID=$row[0]+1;
 
-$sql = "UPDATE num_for_id SET num_for_id ='$ID' WHERE Username = '$name' ";
-    mysqli_query($connect,$sql) or die(mysqli_error());
-
-// $result = mysql_query('SELECT id, badge_name FROM meritbadgestable ORDER BY badge_name');
-
-// while ($row = mysql_fetch_array($result)) 
-// {
-//     $badgenumber = $row['id'];   
+$sql = "UPDATE num_for_id SET Num ='$ID' WHERE Username = '$name' ";
+mysqli_query($connect,$sql) or die(mysqli_error());
+ 
 
 
 mysqli_query($connect,"INSERT INTO Snippets (Username, snippets,ID) VALUES ('$name','$snippet','$ID')");
