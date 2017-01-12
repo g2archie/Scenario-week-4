dde72d94acc4c5c629400d49abe28a2ccb240a41
harrychen0702 <chenhao201509@163.com>
2017-01-12 20:13:23 +0000

    add id

--- scenario/php/newsnippet.php
+++ scenario/php/newsnippet.php
@@ -7,7 +7,20 @@ $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
 $snippet=$_POST['snippet'];
-mysqli_query($connect,"INSERT INTO Snippets (Username, snippets) VALUES ('$name','$snippet')");
+
+$query="SELECT Num FROM num_for_id WHERE Username='$name' ";
+$result=mysqli_query($connect,$query)or die(mysqli_error());
+$row=mysqli_fetch_row($result);
+$ID=$row[0]+1;
+
+// $result = mysql_query('SELECT id, badge_name FROM meritbadgestable ORDER BY badge_name');
+
+// while ($row = mysql_fetch_array($result)) 
+// {
+//     $badgenumber = $row['id'];   
+
+
+mysqli_query($connect,"INSERT INTO Snippets (Username, snippets,ID) VALUES ('$name','$snippet','$ID')");
 header('Location:../pages/home.php');
 
 
