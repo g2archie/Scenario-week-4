cc69a2332c4f2987a6e3f0e792ef27cbcfb9670e
g2archie <g2archie@gmail.com>
2017-01-12 16:09:28 +0000

    Update signin.php

--- scenario/php/signin.php
+++ scenario/php/signin.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect("localhost","root","harry63779588");
+$connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
 
@@ -33,7 +33,7 @@ if (is_correct_password($name, $pw)) {
 
 // query database to see if user typed the right password
 function is_correct_password($name, $pw) {
-	$db = new PDO("mysql:dbname=Scenario4", "root", "harry63779588");
+	$db = new PDO("mysql:dbname=Scenario4", "root", "123456fxf");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$rows = $db->query("SELECT Password FROM userinfo WHERE Username = '$name'");
 	foreach ($rows as $row) {
