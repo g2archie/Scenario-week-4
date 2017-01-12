3a3214d7c7cb146aa50c924088eb6987db0954f7
g2archie <g2archie@gmail.com>
2017-01-12 15:42:11 +0000

    Update signup.php

--- scenario/php/signup.php
+++ scenario/php/signup.php
@@ -2,7 +2,7 @@
 //链接数据库服务器
 // echo (!!@mysqli_connect("localhost","root","harry63779588"));
 $connect=mysql_connect("localhost","root","harry63779588");
-mysqli_select_db("Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
+mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 // $query1="INSERT INTO userinfo (Username, Password) VALUES ('allen','123')";
 
 // $result=mysql_query($query1) or die('sql错误'.mysql_error());
