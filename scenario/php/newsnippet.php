60f23e8f5e51f4e0a425be5ab3d30a5420c221de
g2archie <g2archie@gmail.com>
2017-01-13 21:41:03 +0000

    Update newsnippet.php

--- scenario/php/newsnippet.php
+++ scenario/php/newsnippet.php
@@ -6,10 +6,10 @@ $name=$_SESSION['name'];
 $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
-$snippet=$_POST['snippet'];
+/*$snippet=$_POST['snippet'];
     require './xss.php';
     $xss = new xss_filter();
-    $string = $xss->filter_it($snippet );
+    $string = $xss->filter_it($snippet );*/
 $query="SELECT Num FROM num_for_id WHERE Username='$name' ";
 $result=mysqli_query($connect,$query)or die(mysqli_error());
 $row=mysqli_fetch_row($result);
