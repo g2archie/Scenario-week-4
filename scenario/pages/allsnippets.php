e31cc8ad277778aa6a416e5085ac129599bb3dd3
harrychen0702 <chenhao201509@163.com>
2017-01-13 19:31:12 +0000

    test

--- scenario/pages/allsnippets.php
+++ scenario/pages/allsnippets.php
@@ -35,7 +35,7 @@ $newname=$_SESSION['allsnippet'];
 
     <?php
     // session_start();
-    $newname2=$_SESSION['allsnippet'];
+    $newname2=$_SESSION['name'];
     $connect=mysqli_connect("localhost","root","123456fxf");
     mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //
 
