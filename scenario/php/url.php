c5dbd10fcfd8baf4424c8d890156c6f1f4bbd289
harrychen0702 <chenhao201509@163.com>
2017-01-13 21:15:48 +0000

    fix

--- scenario/php/url.php
+++ scenario/php/url.php
@@ -4,7 +4,7 @@ $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 $rows=mysqli_query($connect,"select * from userinfo where Username='$name' ");
 foreach ($rows as $row ) {
-	$url=$row['homepage_url']
+	$url=$row['homepage_url'];
 }
 echo $url;
 // header('Location:$url');
