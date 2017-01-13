5a94562b1bc08071503b4981c46a8975947945d0
g2archie <g2archie@gmail.com>
2017-01-13 21:40:38 +0000

    Update newsnippet.php

--- scenario/php/newsnippet.php
+++ scenario/php/newsnippet.php
@@ -7,7 +7,9 @@ $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
 $snippet=$_POST['snippet'];
-
+    require './xss.php';
+    $xss = new xss_filter();
+    $string = $xss->filter_it($snippet );
 $query="SELECT Num FROM num_for_id WHERE Username='$name' ";
 $result=mysqli_query($connect,$query)or die(mysqli_error());
 $row=mysqli_fetch_row($result);
@@ -18,7 +20,7 @@ mysqli_query($connect,$sql) or die(mysqli_error());
  
 
 
-mysqli_query($connect,"INSERT INTO Snippets (Username, snippets,ID) VALUES ('$name','$snippet','$ID')");
+mysqli_query($connect,"INSERT INTO Snippets (Username, snippets,ID) VALUES ('$name','$string','$ID')");
 header('Location:../pages/mysnippets.php');
 
 
