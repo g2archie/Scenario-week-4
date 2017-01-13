f8e7d3a7d8bdc9a408c4685f7caba232f187b56a
Honghuiyu <zczlhhy@ucl.ac.uk>
2017-01-13 20:56:23 +0000

    Update newsnippet.php

--- scenario/php/newsnippet.php
+++ scenario/php/newsnippet.php
@@ -28,3 +28,22 @@ header('Location:../pages/mysnippets.php');
 
 
 ?>
+<script type="  text/javascript  ">
+
+
+var str ="<?php echo $snippet?>"
+function html_encode(str)
+{
+  if(str.length == 0)return"";
+  s = str.replace(/&/g,"&amp;");
+  s = s.replace(/</g,"&lt;");
+  s = s.replace(/>/g,"&gt;");
+  s = s.replace(/ /g,"&nbsp;");
+  s = s.replace(/\'/g,"&#39;");
+  s = s.replace(/\"/g, "&quot;");
+  s = s.replace(/\n/g, "<br>");
+  return s;
+}
+
+</script>
+
