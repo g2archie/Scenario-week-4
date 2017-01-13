3da85d918edd5f6f3df94abf107e740f602221b9
harrychen0702 <chenhao201509@163.com>
2017-01-13 22:06:09 +0000

    fix

--- scenario/pages/home.php
+++ scenario/pages/home.php
@@ -27,7 +27,7 @@ session_start();
         <ul class="right hide-on-med-and-down">
             <li><span><?php echo $_SESSION['name'];  ?>  </span></li> 
             <li><a href="editprofile.php">Profile</a></li>
-            <li><a href="../php/signin.php">Sign in</a></li>
+            <li><a href="../php/signin.html">Sign in</a></li>
             <li><a href="../php/logout.php">Sign out</a></li>
         </ul>
     </div>
