77e4483c61d48ba751ec822b6cf6365788b138ef
Honghuiyu <zczlhhy@ucl.ac.uk>
2017-01-13 21:13:27 +0000

    Update newsnippet.php

--- scenario/php/newsnippet.php
+++ scenario/php/newsnippet.php
@@ -7,6 +7,7 @@ $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
 $snippet=$_POST['snippet'];
+?>
 
 
 <script type="  text/javascript  ">
@@ -22,7 +23,7 @@ var str ="<?php echo $snippet?>"
 
   </script>
   
-
+<?php
 $query="SELECT Num FROM num_for_id WHERE Username='$name' ";
 $result=mysqli_query($connect,$query)or die(mysqli_error());
 $row=mysqli_fetch_row($result);
