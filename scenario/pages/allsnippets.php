677e2890f7c69a3ecc7b39071bd563aa642ee595
harrychen0702 <chenhao201509@163.com>
2017-01-13 16:07:40 +0000

    fix

--- scenario/pages/allsnippets.php
+++ scenario/pages/allsnippets.php
@@ -30,42 +30,6 @@ session_start();
 <div class="container">
     <h4><?php echo $_SESSION['allsnippet'];  ?> Snippets:</h4>
 
-    <?php 
-    $connect=mysqli_connect("localhost","root","123456fxf");
-    mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); 
-
-    session_start();
-    $name=$_SESSION['name'];
-    $query="SELECT snippets,ID FROM Snippets WHERE Username='$name' ";
-    $db = new PDO("mysql:dbname=Scenario4", "root", "123456fxf");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $rows = $db->query($query);
-    foreach ($rows as $row) {
-        ?>
-    
-
-    <div class="row">
-        <div class="col s12 m12">
-            <div class="card blue-grey darken-1">
-                <div class="card-content white-text">
-                    <p>
-                        Snippet ID:
-                        <?=$row["ID"] ?>
-                    </p>
-
-                    <p>
-                        <?= $row["snippets"] ?>
-                    </p>
-                </div>
-                
-            </div
-</div>
-
-        </div>
-    </div>
-    <?php
-}
-    ?>
 
     
 </div>
