2dcef6adfd13234b7f9652c355e84bceb53dcecf
g2archie <g2archie@gmail.com>
2017-01-13 18:54:04 +0000

    Update signup.php

--- scenario/php/signup.php
+++ scenario/php/signup.php
@@ -14,7 +14,7 @@ $num=mysqli_num_rows($result);
 if($num > 0){    
    echo "<script type='text/javascript'>alert('Username Already exsits!');location='javascript:history.back()';</script>";    
 }else{    
-mysqli_query($connect,"INSERT INTO userinfo (Username, Password) VALUES ('name','password')"); //将信息添加到userinfo这个表
+mysqli_query($connect,"INSERT INTO userinfo (Username, Password) VALUES ('$name','$password')"); //将信息添加到userinfo这个表
     echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."../pages/return.html"."\""."</script>";
    //echo "<script type='text/javascript'>alert('Registration Successful');</script>";
    //header('Location:../pages/signin.html');
