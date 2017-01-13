77d32a9f73d78c414fa627b2115f6054207f3804
harrychen0702 <chenhao201509@163.com>
2017-01-13 16:10:16 +0000

    test

--- scenario/php/showsnippet.php
+++ scenario/php/showsnippet.php
@@ -1,9 +1,9 @@
 <?php
 $name=$_POST['Username'];
-// echo $name;
-session_start();
-$_SESSION['allsnippet']=$name;
-header('Location:../pages/allsnippets.php');
+echo $name;
+// session_start();
+// $_SESSION['allsnippet']=$name;
+// header('Location:../pages/allsnippets.php');
 
 
 ?>
