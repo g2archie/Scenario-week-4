e0ee8952853d8058d4219ee32c8453db1c2b8a9a
harrychen0702 <chenhao201509@163.com>
2017-01-14 16:19:05 +0000

    reset time

--- scenario/pages/upload.php
+++ scenario/pages/upload.php
@@ -3,7 +3,7 @@ session_start();
 if($_SESSION['checked'] == "" || $_SESSION['checked']==0){
     echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."../pages/signin.php"."\""."</script>";
 }
-$inactive = 10;
+$inactive = 100;
 if( !isset($_SESSION['timeout']) )
 $_SESSION['timeout'] = time() + $inactive; 
 
