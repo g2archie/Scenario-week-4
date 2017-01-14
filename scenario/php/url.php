d4a70a940928290a7f8c8244b640df8aae6095ee
g2archie <g2archie@gmail.com>
2017-01-14 02:32:02 +0000

    Update url.php

--- scenario/php/url.php
+++ scenario/php/url.php
@@ -10,6 +10,7 @@ foreach ($rows as $row ) {
 else
 {
 	echo"<script type="."\""."text/javascript"."\"".">"."window.alert"."("."\""."The user has not set his/her website yet!"."\"".")".";"."</script>";
+        header("Location:../pages/home.php")
 }
 exit();
 
