f112b5735d9294758343edc505d4780173882e3a
harrychen0702 <chenhao201509@163.com>
2017-01-14 20:28:01 +0000

    attack(harry)

--- scenario/php/delete.php
+++ scenario/php/delete.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
+
+// if (isset($_REQUEST['toBankId'] &&　isset($_REQUEST['money']))
+// 　　　　{
+// 　　　　    buy_stocks($_REQUEST['toBankId'],　$_REQUEST['money']);
+// 　　　　}
+
 $name=$_SESSION['name'];
-$ID=$_POST['deleteid'];
+$ID=$_REQUEST['deleteid'];  //修改
 
 $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
