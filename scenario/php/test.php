8aa5b6a2daf15378b6b9510e562a6531c5d6e2c0
g2archie <g2archie@gmail.com>
2017-01-14 03:11:01 +0000

    Update test.php

--- scenario/php/test.php
+++ scenario/php/test.php
@@ -1,13 +1,14 @@
 	<html>
 	<head>
-	<title>Query string</title>
+	<title>Redirecting</title>
 	</head>
 	<body>
 
 	<?php
         $url=$_GET["url"];
 	// The value of the variable name is found
-	echo "<h1>Hello " . $url . "</h1>";
+	//echo "<h1>Hello " . $url . "</h1>";
+	echo "Redirecting";
            echo    '<script type="text/javascript">'
           .     "window.location.href='http://'+'$url';"
           . '</script>'
