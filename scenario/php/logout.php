4717161e73e2a88ef60f09543ba2c72b8cb84060
Honghuiyu <zczlhhy@ucl.ac.uk>
2017-01-14 20:07:25 +0000

    Update logout.php

--- scenario/php/logout.php
+++ scenario/php/logout.php
@@ -2,5 +2,5 @@
 session_start();
 unset($_SESSION["name"]);
 unset($_SESSION["allsnippet"]); // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
-header("Location: ../pages/sign.php");
+header("Location: ../pages/signin.php");
 ?>
