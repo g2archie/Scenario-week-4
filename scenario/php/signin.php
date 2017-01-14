c9c4accf03f59d6d206222164d166f5e3f08136a
Honghuiyu <zczlhhy@ucl.ac.uk>
2017-01-14 18:10:26 +0000

    Update signin.php

--- scenario/php/signin.php
+++ scenario/php/signin.php
@@ -2,7 +2,8 @@
 $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
-
+ $name = $_POST["user_name"];
+  $pw = $_POST["password"];
 
 $sql = "SELECT Username FROM userinfo WHERE username='{$name}' AND password='{$pw}'";
   $query = mysqli_query($connect,$sql);
