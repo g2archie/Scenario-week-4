55aa1f380601ddfc0c3c339b1038c7f94c1d783b
harrychen0702 <chenhao201509@163.com>
2017-01-14 15:49:10 +0000

    fix

--- scenario/php/logout.php
+++ scenario/php/logout.php
@@ -2,5 +2,6 @@
 session_start();
 unset($_SESSION["name"]);
 unset($_SESSION["allsnippet"]); // where $_SESSION["nome"] is your own variable. if you do not have one use only this as follow **session_unset();**
+unset($_SESSION["timeout"]);
 header("Location: ../pages/signin.php");
 ?>
