8148ffc17818108776cf0f49c100570581b05db0
harrychen0702 <chenhao201509@163.com>
2017-01-14 15:40:43 +0000

    modify inactive time to 10mins

--- scenario/pages/allsnippets.php
+++ scenario/pages/allsnippets.php
@@ -4,7 +4,7 @@ if($_SESSION['checked'] == "" || $_SESSION['checked']==0){
     echo"<script type="."\""."text/javascript"."\"".">"."window.location="."\""."../pages/signin.php"."\""."</script>";
 }
 
-$inactive = 0.5;
+$inactive = 10;
 if( !isset($_SESSION['timeout']) )
 $_SESSION['timeout'] = time() + $inactive; 
 
