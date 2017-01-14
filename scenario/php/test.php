7bcad553d3da7df9f769a64c37855a76c3d9a175
g2archie <g2archie@gmail.com>
2017-01-14 02:37:22 +0000

    Create test.php

---
+++ scenario/php/test.php
@@ -0,0 +1,16 @@
+	<html>
+	<head>
+	<title>Query string</title>
+	</head>
+	<body>
+
+	<?php
+
+	// The value of the variable name is found
+	echo "<h1>Hello " . $_GET["url"] . "</h1>";
+
+	?>
+
+	</body>
+	</html>
+	
