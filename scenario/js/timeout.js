7f559969c450dad90bfacc0c32b8272bc12543cb
Honghuiyu <zczlhhy@ucl.ac.uk>
2017-01-14 16:34:49 +0000

    Delete timeout.js

--- scenario/js/timeout.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  setTimeout(function(){
-        $.get("../php/check.php", function(data){
-        if(data==0) window.location.href="../php/logout.php";
-        });
-    },1*60*1000);
-});
