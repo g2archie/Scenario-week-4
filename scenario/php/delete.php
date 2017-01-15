12986a34e48091873cd49e8698564da44c758438
harrychen0702 <chenhao201509@163.com>
2017-01-15 20:13:13 +0000

    un bugged

--- scenario/php/delete.php
+++ scenario/php/delete.php
@@ -1,23 +1,23 @@
 <?php
 session_start();
 
-// if(isset($_POST['check'])) {
-// $hash = md5($_COOKIE['cookie']);
-// if($_POST['check'] == $hash) {
-// $name=$_SESSION['name'];
-// $ID=$_REQUEST['deleteid'];  //制造漏洞
+if(isset($_POST['check'])) {
+$hash = md5($_COOKIE['cookie']);
+if($_POST['check'] == $hash) {
+$name=$_SESSION['name'];
+$ID=$_REQUEST['deleteid'];  //制造漏洞
 
-// $connect=mysqli_connect("localhost","root","123456fxf");
-// mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
+$connect=mysqli_connect("localhost","root","123456fxf");
+mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 
-// mysqli_query($connect,"DELETE FROM `Snippets` WHERE Username='$name' AND ID='$ID' "); //
+mysqli_query($connect,"DELETE FROM `Snippets` WHERE Username='$name' AND ID='$ID' "); //
 
-// header('Location:../pages/mysnippets.php');} 
-// else {
-// 　　　　　　　　header("Location:../pages/signin.php");}} 
-// else {
-// 　　　　　　header("Location:../pages/signin.php");
-// }
+header('Location:../pages/mysnippets.php');} 
+else {
+　　　　　　　　header("Location:../pages/signin.php");}} 
+else {
+　　　　　　header("Location:../pages/signin.php");
+}
 
 
 
