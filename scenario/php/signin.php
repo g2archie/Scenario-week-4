62a126d8f0bbeb7fc5372dd791302d973e91b951
g2archie <g2archie@gmail.com>
2017-01-15 15:34:20 +0000

    Update signin.php

--- scenario/php/signin.php
+++ scenario/php/signin.php
@@ -3,7 +3,7 @@ $connect=mysqli_connect("localhost","root","123456fxf");
 mysqli_select_db($connect,"Scenario4") or die('数据库连接错误，错误信息：'.mysqli_error()); //链接到Scenario4数据库
 session_start();
  $name = $_POST["user_name"];
- $pw = md5($_POST["password"]);
+ $pw = password_hash($_POST["password"],PASSWORD_DEFAULT);
 
 
 
